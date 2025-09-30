6d8faa1445583c6ffdcd0d8a260627c4a7725c55
mcahoon-coder <mcahoon@richardwinn.org>
2025-09-30 08:24:34 -0400

    Update app.py

--- app.py
+++ app.py
@@ -57,7 +57,7 @@ MAIL_SERVER = os.environ.get("MAIL_HOST", "smtp.gmail.com")
 MAIL_PORT = int(os.environ.get("MAIL_PORT", 587))
 MAIL_USE_TLS = os.environ.get("MAIL_USE_TLS", "TRUE").lower() in ("true", "1", "yes")
 MAIL_USE_SSL = False
-MAIL_USERNAME = os.environ.get("MAIL_USER")
+MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
 MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
 MAIL_DEFAULT_SENDER = os.environ.get("MAIL_USER")
 
